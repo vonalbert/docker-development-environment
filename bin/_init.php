dd3bb4f9bce0f443ad699d535a736f362292d2fa
Alberto Avon <alberto.avon@gmail.com>
2018-11-30 08:23:00 +0100

    Moved generation script in a function

--- bin/_init.php
+++ bin/_init.php
@@ -14,6 +14,28 @@ if (!function_exists('docker_compose')) {
 if (!function_exists('docker_compose_generate')) {
     function docker_compose_generate()
     {
-        exec(__DIR__ . '/generate');
+        $config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
+
+        $compose = [
+            'version' => '3.7',
+            'services' => [],
+        ];
+
+
+        include __DIR__ . '/../docker/phpmyadmin/load.php';
+        include __DIR__ . '/../docker/pgadmin/load.php';
+        include __DIR__ . '/../docker/mysql/load.php';
+        include __DIR__ . '/../docker/postgres/load.php';
+
+        // Generate docker compose file
+        $file = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+        $content = json_encode($compose, JSON_UNESCAPED_SLASHES);
+
+        if (file_put_contents($file, $content) === false) {
+            throw new \RuntimeException(sprintf(
+                'Cannot write content to %s file',
+                $file
+            ));
+        }
     }
 }
